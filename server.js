fc08aed7c5cbfa99fe3c31edd9eade6580004af4
“Hilal-Tapan“ <hilalt98@hotmail.com>
2022-03-14 11:29:52 +0100

    Heroku

--- server.js
+++ server.js
@@ -3,12 +3,33 @@ const camelCase = require('camelCase');
 const dotenv = require('dotenv').config();
 var path = require('path');
 
-const { MongoClient } = require('mongodb');
-const { ObjectId } = require('mongodb');
+const {
+  MongoClient
+} = require('mongodb');
+const {
+  ObjectId
+} = require('mongodb');
 
 const app = express();
 const port = 5000;
 let db = null;
+
+/*****************************************************
+ * Connect to database
+ ****************************************************/
+async function connectDB() {
+  const uri = process.env.DB_URI;
+  const client = new MongoClient(uri, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  });
+  try {
+    await client.connect();
+    db = client.db(process.env.DB_NAME);
+  } catch (error) {
+    throw error;
+  }
+}
 
 console.log(process.env.TESTVAR);
 
@@ -19,41 +40,49 @@ app.use(express.static('static'));
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
 
-  // CAROUSEL
-  // app.get('/', (req, res) => {res.render('Carousel', {images});
-// });
 
+//Carousel
 app.get('/', async (req, res) => {
   //FIND IMAGE
   const images = await db.collection('Images').find({}).toArray();
-  res.render('Carousel', {images});
-
-  // // FIND IMAGE
-  //   const id = req.params.ImageId;
-  //   const query = {_id: ObjectId(req.params.movieId)}
-  //   const Image = await db.collection('Image').findOne(query);
+  res.render('Carousel', {
+    images
+  });
 });
 
-// review
+
+// review 1
 app.get('/image/:id', async (req, res) => {
   const imageId = req.params.id;
-  const query = {"id": imageId }
-  // const Reviews = await db.collection('Reviews').find({}).toArray();
-  const image = await db.collection('Images').findOne( query)
-  res.render('review.ejs', {image});
+  const query = {
+    "id": imageId
+  };
+  const image = await db.collection('Images').findOne(query)
+  // res.render('review.ejs', {
+  //   image
+  app.get('/review', async (req, res) => {
+    res.render('review.ejs', {
+      image
+  });
 });
+});
+
+//review 2
+// app.get('/review', async (req, res) => {
+//   res.render('review.ejs', {
+//     image
+//   });
+// });
 
 // Add review sturen
 app.post('/review', async (req, res) => {
   console.log("Hij doet hetttttttt");
   const review = {
     "id": req.body.imageId,
-    "review": req.body.reviewtje
+    "review": req.body.review
   };
   await db.collection('Reviews').insertOne(review);
 });
-
-//
 
 
 // logIn
@@ -70,7 +99,7 @@ app.get('/account', (req, res) => {
   });
 });
 
-//Favorieten
+//favorieten
 app.get('/favorieten', (req, res) => {
   res.render('favorieten.ejs', {
     root: __dirname
@@ -97,30 +126,15 @@ app.use((req, res) => {
 
 
 /*****************************************************
-* Connect to database
-****************************************************/
-async function connectDB() {
-  const uri = process.env.DB_URI;
-  const client = new MongoClient(uri, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-  });
-  try {
-  await client.connect();
-  db = client.db(process.env.DB_NAME);
-  } catch (error) {
-  throw error;
-  }
-  }
-
-/*****************************************************
-* Start webserver
-****************************************************/
+ * Start webserver
+ ****************************************************/
 
 app.listen(port, () => {
   console.log('=============================================\n\n')
-  console.log('Webserver running on http://localhost:${port}\n\n')
+  console.log(`Webserver running on http://localhost:${port}\n\n`)
   console.log('=============================================\n\n')
 
-  connectDB().then( () => console.log("We have a connection to mongo!"));
-});
+  connectDB()
+    .then(() => console.log("We have a connection to mongo!"))
+    .catch(err => console.log(err))
+  });
