afec92a5e8fd6fde50901b18f65f4903890012b4
“Hilal-Tapan“ <hilalt98@hotmail.com>
2022-03-14 12:13:49 +0100

    port

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ const {
 } = require('mongodb');
 
 const app = express();
-const port = 5000;
+const port = process.env.PORT || 5000;
 let db = null;
 
 /*****************************************************
