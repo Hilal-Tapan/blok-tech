41a6f7bebc7c776f5990483c9b9d9296b7d0f397
“Hilal-Tapan“ <hilalt98@hotmail.com>
2022-04-10 19:53:12 +0200

    puntjes op de i

--- static/api.js
+++ static/api.js
@@ -1,9 +1,12 @@
 // // // API
+// Fetchen van de API
 fetch('https://www.fakerestapi.com/datasets/api/v1/mnc-companies-reviews.json')
     .then(res => res.json())
     .then(data => {
+        // Slicen, ik wil er maar 3 zien.
         const threeReviews = data.data.slice(0, 3);
 
+         // Loopen met forEach
         threeReviews.forEach(review => {
 
             const reviewHtml =
@@ -11,10 +14,8 @@ fetch('https://www.fakerestapi.com/datasets/api/v1/mnc-companies-reviews.json')
                 <h2 class="review-name">${review.reviewed_by}</h2>
                 <p class="review-description">${review.review}</p>  
                 `
-
+            // Een container voor in de HTML aanmaken
             const reviewContainer = document.getElementById('apiReviews')
             reviewContainer.innerHTML += reviewHtml
-
-
         });
     });
